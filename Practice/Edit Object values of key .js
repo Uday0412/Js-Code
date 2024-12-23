32d2e2520a23b8d150cdcd55c39a170a058e21e4
uday kushwaha <145842199+Uday0412@users.noreply.github.com>
2024-12-23 08:08:03 +0530

    click here to see code

---
+++ Practice/Edit Object values of key .js
@@ -0,0 +1,13 @@
+const person = {
+    person_name : "ABC",
+    Age : 12,
+    City : "XYZ",
+};
+
+// we edit the city name
+person.City = "GEf";
+console.log("the city of person: ",person.City);
+
+// we add the country in person obejct
+person.country = "United Sates";
+console.log("the counter of person: ",person.country);
