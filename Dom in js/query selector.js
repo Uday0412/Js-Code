ce1560fd389467a92c4478c839084024228ab093
uday kushwaha <145842199+Uday0412@users.noreply.github.com>
2025-01-08 08:01:57 +0530

    inital commit

---
+++ Dom in js/query selector.js
@@ -0,0 +1,15 @@
+// queryselector by selecting tag name
+let num = document.querySelector('p');
+console.log("the output of selecting tag name: ",num);
+
+// queryselector by using selecting id
+let num1 = document.querySelector("#p1");
+console.log("the output of selecting id name: ",num1);
+
+// queryselector by using class 
+let num2 = document.querySelector(".p2");
+console.log("the output of selecting class name: ",num2);
+
+// queryselector by using all
+let num3 = document.querySelectorAll("p3");
+console.log("the output of selecting all: ",num3);
