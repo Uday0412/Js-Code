2f11a4ebf727607e6e40d9120c8dab186c1b0ea6
uday kushwaha <145842199+Uday0412@users.noreply.github.com>
2025-01-08 08:09:46 +0530

    intial commit

---
+++ Dom in js/Manupulating Attributes.js
@@ -0,0 +1,7 @@
+// by using get attribute
+let num1 = Object.getattribute(attribute_name);
+console.log("the output of getattribute : ".num1);
+
+// by using set attribute
+let num2 = Object.setattribute(attribute_name,val_name);
+console.log("the output of getattribute: ",num2);
