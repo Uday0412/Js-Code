24f9bd12884ae39571658f738f4663655acee75d
uday kushwaha <145842199+Uday0412@users.noreply.github.com>
2025-01-08 07:50:17 +0530

    inital commit

---
+++ Dom in js/selecting elements from html page.js
@@ -0,0 +1,13 @@
+// selecting with document get element by id
+let h1 = document.getElementById("h1");
+
+console.log("the output= ",h1);
+
+// selecting with get element by class name
+let h2 = document.getElementsByClassName("h2");
+console.log("the output of h2 heading: ",h2);
+
+// selecting with using get element by tag name
+let h3 = document.getElementsByTagName("h3");
+
+cosole.log("the output of h3 heading : ",h3);
