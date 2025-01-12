fb9aa2df21f0cbd3bc333c4ecfa0ca58b9b96d24
uday kushwaha <145842199+Uday0412@users.noreply.github.com>
2025-01-12 08:10:25 +0530

    intial commit

---
+++ Dom in js/Events/Event listners/Event listners.js
@@ -0,0 +1,9 @@
+let btn = document.querySelector("button");
+
+// this is use only for one button if you can use for multiple
+// buttons the you can you for of loop to select all this button 
+// you can made in html code.
+
+btn.addEventListener("click",function(){
+    alert("button was clicked");
+});
